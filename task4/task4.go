b1c78142a376460477ce4d692fc7fb0ed416ca47
eashabalin <egorsabalin@MacBook-Pro-Egor-2.local>
2023-03-23 19:22:30 +0500

    task4 done

--- task4/task4.go
+++ task4/task4.go
@@ -3,39 +3,73 @@ package task4
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
-func Worker(id int, jobs <-chan int, results chan<- bool) {
+// Worker – функция, которая будет запускаться в нескольких горутинах
+func Worker(id int, jobs <-chan int) {
 	for j := range jobs {
 		time.Sleep(time.Second)
 		fmt.Println("Worker", id, ": ", j)
-		results <- true
 	}
 }
 
-func GenerateJobs(jobs chan<- int) {
-	for {
-		jobs <- rand.Intn(100)
-		time.Sleep(100 * time.Millisecond)
+// GenerateJobs – постоянно публикует данные в канал jobs до тех пор, пока не будет получен сигнал в канале done
+func GenerateJobs(jobs chan<- int, done <-chan bool) {
+	for i := 1; true; i++ {
+		select {
+		case <-done:
+			close(jobs)
+			return
+		default:
+			fmt.Println(i)
+			jobs <- rand.Intn(100)
+			time.Sleep(100 * time.Millisecond)
+		}
 	}
 }
 
 func Run() {
 	fmt.Println("--- Task 4 ---")
 
-	const numJobs = 5
+	// Количество воркеров
 	const numWorkers = 5
-	jobs := make(chan int, numJobs)
-	results := make(chan bool, numJobs)
 
+	// Канал, в который будут приходить данные
+	jobs := make(chan int, 100)
+
+	// WaitGroup нужна для того, чтобы программа не завершалась до тех пор, пока все воркеры завершат работу
+	wg := sync.WaitGroup{}
+
+	// Запуск воркеров
 	for w := 1; w <= numWorkers; w++ {
-		go Worker(w, jobs, results)
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			Worker(id, jobs)
+		}(w)
 	}
 
-	GenerateJobs(jobs)
+	// Канал для завершения публикации данных
+	done := make(chan bool, 1)
 
-	for {
-		<-results
-	}
+	// Запуск публикации данных в канал jobs в отдельной горутине
+	go GenerateJobs(jobs, done)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// Ожидание сигнала завершения программы из ОС (Ctrl+C)
+	<-sigs
+
+	// Посылаем сигнал завершения публикации в канал jobs
+	done <- true
+
+	// Ждём пока все воркеры завершат работу
+	wg.Wait()
+
 }
